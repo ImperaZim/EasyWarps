7029b640f7c184ca4226ab83b77d479fabd5729d
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-10-07 17:48:30 -0300

    update WarpCommand.php

--- src/ImperaZim/EasyWarps/Commands/WarpCommand.php
+++ src/ImperaZim/EasyWarps/Commands/WarpCommand.php
@@ -2,12 +2,13 @@
 
 namespace ImperaZim\EasyWarps\Commands;
 
-use ImperaZim\EasyWarps\Loader;
 use pocketmine\Server;
 use pocketmine\player\Player;
 use pocketmine\command\Command;
+use ImperaZim\EasyWarps\Loader;
 use pocketmine\plugin\PluginOwned;
 use pocketmine\command\CommandSender;
+use ImperaZim\EasyWarps\Functions\WarpCreate;
 
 class WarpCommand extends Command implements PluginOwned {
 
@@ -18,6 +19,22 @@ class WarpCommand extends Command implements PluginOwned {
  
  public function execute(CommandSender $player, string $commandLabel, array $args) : bool {
   $plugin = $this->getOwningPlugin();
+  if (isset($args[0])) {
+   switch ($args[0]) {
+    case 'create': case 'criar':
+     if ($player->hasPermission("warp.create")) {
+      if (!isset($args[1])) {
+       return true;
+      }
+      WarpCreate::execute($player, $name, $permission);
+     }else{
+      $player->sendMessage("§l§cWARP§r Você não tem permissão para criar uma warp!");
+     }
+     break;
+   }
+  }else{
+   $player->sendMessage("§l§cWARP§r Argumento invalido! Use /warp (criar, deletar)!");
+  }
  }
  
  public function getOwningPlugin(): Loader {
