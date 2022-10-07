1b5e43e8275436802e03425582204d046e62e01c
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-10-07 17:08:13 -0300

    update WarpCommand.php

--- src/ImperaZim/EasyWarps/Commands/WarpCommand.php
+++ src/ImperaZim/EasyWarps/Commands/WarpCommand.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace ImperaZim\EasyWarps\Commands;
+
+use ImperaZim\EasyWarps\Loader;
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\command\Command;
+use pocketmine\plugin\PluginOwned;
+use pocketmine\command\CommandSender;
+
+class WarpCommand extends Command implements PluginOwned {
+
+ public function __construct() {
+  parent::__construct("warp", "§7Warp command!", null, []);
+		$this->setPermission("warp.command"); 
+ }
+ 
+ public function execute(CommandSender $player, string $commandLabel, array $args) : bool {
+  $plugin = $this->getOwningPlugin();
+ }
+ 
+ public function getOwningPlugin(): Loader {
+		return Loader::getInstance();
+	}  
+ 
+}
