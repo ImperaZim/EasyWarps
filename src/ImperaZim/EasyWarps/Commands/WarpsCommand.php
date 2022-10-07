f9f2db07d6ffa4d51db2d779bf81ce6ab59a85ad
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-10-07 18:14:29 -0300

    update WarpsCommand.php

--- src/ImperaZim/EasyWarps/Commands/WarpsCommand.php
+++ src/ImperaZim/EasyWarps/Commands/WarpsCommand.php
@@ -1,5 +1,20 @@
 <?php
 
 class WarpsCommand extends Command implements PluginOwned {
+ 
+ public function __construct() {
+  parent::__construct("warps", "§7Lista de warps!", null, []);
+		$this->setPermission("warp.command"); 
+ }
+ 
+ public function execute(CommandSender $player, string $commandLabel, array $args) : bool {
+  $plugin = $this->getOwningPlugin(); 
+  
+  return true;
+ }
+ 
+ public function getOwningPlugin(): Loader {
+		return Loader::getInstance();
+	}  
 
 }
