5f4b34c95b467202680cf0bcbc8e9a292ca248a3
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-10-07 18:00:53 -0300

    update WarpDelete.php

--- src/ImperaZim/EasyWarps/Functions/WarpDelete.php
+++ src/ImperaZim/EasyWarps/Functions/WarpDelete.php
@@ -1,1 +1,24 @@
+<?php
 
+namespace ImperaZim\EasyWarps\Functions;
+
+use pocketmine\utils\Config;
+use ImperaZim\EasyWarps\Loader;
+
+class WarpCreate {
+ 
+ public static function execute($player, $name) : void {
+  $plugin = Loader::getInstance();
+  $config = new Config($plugin->getDataFolder() . "warps.yml");
+  $data = $plugin->getDataFolder();
+  
+  if (!isset($config->getAll()[$name])) {
+   $player->sendMessage("§l§cWARP§r Está warp não existe!");
+   return true;
+  }else{ 
+   
+   $player->sendMessage("§l§cWARP§r Warp {$name} deletada com sucesso!");
+  }
+ }
+ 
+}
