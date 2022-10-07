4745af9fb5ace8df263762c4ecba4c8c68688042
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-10-07 16:54:32 -0300

    Update Loader.php

--- src/ImperaZim/EasyWarps/Loader.php
+++ src/ImperaZim/EasyWarps/Loader.php
@@ -2,6 +2,7 @@
 
 namespace ImperaZim\EasyWarps;
 
+use pocketmine\Server;
 use pocketmine\plugin\PluginBase;
 
 class Loader extends PluginBase {
@@ -14,22 +15,14 @@ class Loader extends PluginBase {
   
   public function onEnable() : void {
     self::$instance = $this;
+    self::registerWorlds();
   }
-  
-  public function loadWorlds(){
 
-$levelNamesArray = scandir($this->getServer()->getDataPath() . "worlds/");
-
-  foreach($levelNamesArray as $levelName){
-
-   if($levelName === "." || $levelName === ".."){continue;}
-
-   $this->getServer()->getWorldManager()->loadWorld($levelName);
-
-  } $levels = $this->getServer()->getWorldManager()->getWorlds(); 
-
+  public static function registerWorlds() : void {
+  $worlds = scandir(->getDataPath() . "worlds/");
+  foreach($worlds as $world){
+   Server::getinstance()->getWorldManager()->loadWorld($world);
+  } 
  }   
   
-} me);
-  } $levels = $this->getServer()->getWorldManager()->getWorlds(); 
- }  
+}  
