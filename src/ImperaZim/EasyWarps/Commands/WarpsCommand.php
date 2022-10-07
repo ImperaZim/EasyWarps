75b426b6a2dac295f5aa91fa4774d78a4e6a6d46
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-10-07 18:19:34 -0300

    update WarpsCommand.php

--- src/ImperaZim/EasyWarps/Commands/WarpsCommand.php
+++ src/ImperaZim/EasyWarps/Commands/WarpsCommand.php
@@ -1,15 +1,23 @@
 <?php
 
+use pocketmine\utils\Config;
+use pocketmine\player\Player;
+use ImperaZim\EasyWarps\Loader;
+
 class WarpsCommand extends Command implements PluginOwned {
  
  public function __construct() {
   parent::__construct("warps", "§7Lista de warps!", null, []);
-		$this->setPermission("warp.command"); 
+		$this->setPermission("warps.command"); 
  }
  
  public function execute(CommandSender $player, string $commandLabel, array $args) : bool {
   $plugin = $this->getOwningPlugin(); 
-  
+  if (!$player instanceof Player) {
+   $plugin->getLogger()->warning("Este comando está desabilitado no console! Tente usa-lo dentro do jogo!");
+   return true;
+  }
+  //send Menu
   return true;
  }
  
